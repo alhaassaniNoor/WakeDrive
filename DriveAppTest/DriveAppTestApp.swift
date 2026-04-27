5004af7f6198b39ec13598d9f1743f93bbf9d0bb
jurishogair <jouryshogair1@gmail.com>
2026-04-27 14:14:04 +0300

    Initial Commit

---
+++ DriveAppTest/DriveAppTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DriveAppTestApp.swift
+//  DriveAppTest
+//
+//  Created by juri shogair on 10/11/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct DriveAppTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DriveAppTestTests/DriveAppTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  DriveAppTestTests.swift
+//  DriveAppTestTests
+//
+//  Created by juri shogair on 10/11/1447 AH.
+//
+
+import Testing
+@testable import DriveAppTest
+
+struct DriveAppTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
